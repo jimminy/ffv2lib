aa2109574a27b718584aded127e15889bba35b88
James Fuller <jimminyfuller@gmail.com>
2012-03-28 02:11:43 -0400

    Removed app files to barebones the component

--- config.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-define('CONSUMER_KEY','xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
-define('CONSUMER_SECRET', 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
-define('UA', 'FFv2-API/v0.4');
